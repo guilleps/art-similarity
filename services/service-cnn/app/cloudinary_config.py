4f1ad32e67c496b9be8750947e56a95ea9d887cf
Guillermo Palacios <nothicc04@gmail.com>
2025-06-13 00:27:02 -0500

    feat: Update Dockerfile and main application for FastAPI integration; modify Cloudinary config and requirements

--- services/service-cnn/app/cloudinary_config.py
+++ services/service-cnn/app/cloudinary_config.py
@@ -2,10 +2,14 @@ import os
 import cloudinary
 import cloudinary.uploader
 
+CLOUDINARY_CLOUD_NAME = ""
+CLOUDINARY_API_KEY = ""
+CLOUDINARY_API_SECRET = ""
+
 cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    cloud_name=CLOUDINARY_CLOUD_NAME,
+    api_key=CLOUDINARY_API_KEY,
+    api_secret=CLOUDINARY_API_SECRET,
     secure=True
 )
 
