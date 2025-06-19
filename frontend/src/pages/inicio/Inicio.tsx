8358fafe40405632e6f839bb2557d27a9f885878
Guillermo Palacios <nothicc04@gmail.com>
2025-06-19 00:05:52 -0500

    feat: implement auto-scroll functionality and refactor related components for improved user experience

--- frontend/src/pages/inicio/Inicio.tsx
+++ frontend/src/pages/inicio/Inicio.tsx
@@ -3,7 +3,7 @@ import { BasicTerms } from "./BasicTerms";
 import { HeroSection } from "./HeroSection";
 import { ProblemSection } from "./ProblemSection";
 
-export const Inicio = ({ basicTermOne, basicTermTwo, basicTermThree, selectedTerm }) => {
+export const Inicio = () => {
   return (
     <div className="space-y-0">
       <Navbar />
@@ -12,12 +12,7 @@ export const Inicio = ({ basicTermOne, basicTermTwo, basicTermThree, selectedTer
       
       <ProblemSection />
       
-      <BasicTerms
-        selectedTerm={selectedTerm}
-        onSelectTermOne={basicTermOne}
-        onSelectTermTwo={basicTermTwo}
-        onSelectTermThree={basicTermThree}
-      />
+      <BasicTerms />
     </div>
   );
 };
