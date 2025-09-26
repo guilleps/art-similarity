477649b7a9bd775ca255dfc9f9c15bcbdc87bc81
Guillermo Palacios <nothicc04@gmail.com>
2025-09-26 15:29:13 -0500

    feat: init model efficientnet B2 and B1

---
+++ tesis_experimentation_v4/fine-tunning/main.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import keras
+from keras.applications import EfficientNetB2
+
+IMG_SIZE = 256
+BATCH_SIZE = 64
+
+model = EfficientNetB2(
+    include_top=False, 
+    weights=None,
+    input_shape=(IMG_SIZE, IMG_SIZE, 3)
+)
+
+print("Backend actual:", keras.backend.backend())
+print("Número de capas EfficientNetB2:", len(model.layers))
