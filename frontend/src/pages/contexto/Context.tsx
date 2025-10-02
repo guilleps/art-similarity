f9e2d7a574e1c1b12388aa08df8786ff03bedb41
Guillermo Palacios <nothicc04@gmail.com>
2025-10-02 11:52:20 -0500

    fix: correct JSON key from "par" to "pair" and remove unused images

--- frontend/src/pages/contexto/Context.tsx
+++ frontend/src/pages/contexto/Context.tsx
@@ -1,11 +1,8 @@
-import { useAutoScrollView } from '@/hooks/useAutoScrollView';
 import { Navbar } from '../../components/Navbar';
 import { About } from './About';
 import { FlowSection } from './FlowSection';
 
 export const Context = () => {
-	useAutoScrollView();
-
 	return (
 		<div className="space-y-0">
 			<Navbar />
