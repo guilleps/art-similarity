9540448c08c03535dc6198d5cbae955f9d0684e5
Guillermo Palacios <nothicc04@gmail.com>
2025-10-13 17:41:07 -0500

    feat: add health check endpoint to backend service

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,29 @@
-
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
+
+def health_check(request):
+    from django.http import JsonResponse
+
+    return JsonResponse({"status": "ok", "message": "The backend service is running."})
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
+    path("", health_check),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+    ),
 ]
