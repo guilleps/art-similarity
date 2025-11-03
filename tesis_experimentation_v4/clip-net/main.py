a5161501f52a864447ab508a25ce1b17b08c2a2a
Guillermo Palacios <nothicc04@gmail.com>
2025-11-03 10:49:52 -0500

    feat: clean up and reorganize codebase by removing unused files and adding necessary .gitignore entries

--- tesis_experimentation_v4/clip-net/main.py
+++ tesis_experimentation_v4/clip-net/main.py
@@ -1,47 +1,27 @@
-import os
-from cnn_ import run_pipeline
-from cosine_similarity import compare_all_embeddings
-import logging
-from logger_config import setup_logging
+import torch
+from PIL import Image
+from torchvision import transforms
+from transformers import CLIPProcessor, CLIPModel
 
-setup_logging()
-logger = logging.getLogger(__name__)
+# Cargar modelo y procesador
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32", use_fast=False)
 
-def find_image_folders(base_folder):
-    image_folders = []
-    for root, dirs, files in os.walk(base_folder):
-        if any(file.lower().endswith(('.png', '.jpg', '.jpeg')) for file in files):
-            image_folders.append(root)
-    return image_folders
+# Cargar imagen
+image = Image.open("gato.jpeg")
 
-def main():
-    base_folder = r"C:\workspace\tesis_project\tesis_experimentation_v4\clip-net\images\1"
-    embeddings_dir = os.path.join(base_folder, "embeddings")
-    transformed_dir = os.path.join(base_folder, "transformed_images")
-    output_json_path = os.path.join(embeddings_dir, "resultados_similitud.json")
-    
-    # Ejecucion de pipeline de transformaciones y extracción de embeddings
-    logger.info("Inicio del pipeline...")
-    run_pipeline(base_folder, transformed_dir, embeddings_dir)
+# Lista de textos candidatos
+texts = ["a dog", "a cat", "a bird"]
 
-    logger.info("Buscando carpetas con embeddings...")
-    embedding_folders = [
-        os.path.join(embeddings_dir, folder) 
-        for folder in os.listdir(embeddings_dir) 
-        if os.path.isdir(os.path.join(embeddings_dir, folder))
-    ]
+# Preprocesar inputs
+inputs = processor(text=texts, images=image, return_tensors="pt", padding=True)
 
-    # Paso 3: Comparar todos los pares de embeddings (evitando comparar consigo mismo)
-    logger.info("Comparando similitudes...")
-    for i in range(len(embedding_folders)):
-        for j in range(i + 1, len(embedding_folders)):
-            compare_all_embeddings(
-                embedding_folders[i], 
-                embedding_folders[j], 
-                output_json_path
-            )
-    
-    logger.info("Pipeline completado")
+# Obtener similitudes
+with torch.no_grad():
+    outputs = model(**inputs)
+    logits_per_image = outputs.logits_per_image  # [1, N textos]
+    probs = logits_per_image.softmax(dim=1)      # Probabilidades
 
-if __name__ == "__main__":
-    main()
+# Mostrar resultados
+for label, prob in zip(texts, probs[0]):
+    print(f"{label}: {prob:.4f}")
